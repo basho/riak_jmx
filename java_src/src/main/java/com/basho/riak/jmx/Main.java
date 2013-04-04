871cdbff4cd6ef82b1e582ed1954edb57bcb1b1d
Joe DeVivo <joe.devivo@basho.com>
2013-04-04 08:39:25 -0700

    Stats are now something that update... for some datatypes

--- java_src/src/main/java/com/basho/riak/jmx/Main.java
+++ java_src/src/main/java/com/basho/riak/jmx/Main.java
@@ -2,6 +2,7 @@ package com.basho.riak.jmx;
 
 import java.lang.management.ManagementFactory;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.management.MBeanServer;
@@ -10,6 +11,7 @@ import javax.management.ObjectName;
 import com.ericsson.otp.erlang.*;
 
 import javassist.*;
+import java.lang.reflect.*;
 
 public class Main {
 
@@ -24,20 +26,15 @@ public class Main {
         OtpPeer riak  = new OtpPeer(node); 
         OtpConnection connection = self.connect(riak); 
 
-        connection.sendRPC("riak_jmx", "stats", new OtpErlangList());
-        OtpErlangList stats = (OtpErlangList)connection.receiveRPC(); 
-        
+        OtpErlangList stats = getStats(connection); 
+
         makeBeanClass(stats);
 
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName name = new ObjectName("com.basho.riak:type=Riak");
-        RiakMBean mbean = new Riak();
-        mbs.registerMBean(mbean, name);
-        Riak r = (Riak)mbean;
+        Riak r = createAndRegisterMBean();
 
         while (true) {
+            update(r, connection);
             Thread.sleep(30000);
-            update(r);
         }
     }
     
@@ -52,28 +49,27 @@ public class Main {
             OtpErlangTuple tuple = (OtpErlangTuple)it.next();
             OtpErlangAtom name = (OtpErlangAtom)tuple.elementAt(0);
             OtpErlangObject value = tuple.elementAt(1);
-            System.out.println(name);
 
             CtClass fieldType = pool.get("java.lang.Object");
 
             if(value instanceof OtpErlangFloat) {
-                fieldType = CtClass.floatType;
+                fieldType = pool.get("java.lang.Float");
             } else if (value instanceof OtpErlangDouble) {
-                fieldType = CtClass.doubleType;
+                fieldType = pool.get("java.lang.Double");
             } else if (value instanceof OtpErlangByte) {
-                fieldType = CtClass.byteType;
+                fieldType = pool.get("java.lang.Byte");
             } else if (value instanceof OtpErlangChar) {
-                fieldType = CtClass.charType;
+                fieldType = pool.get("java.lang.Character");
             } else if (value instanceof OtpErlangShort) {
-                fieldType = CtClass.shortType;
+                fieldType = pool.get("java.lang.Long");
             } else if (value instanceof OtpErlangUShort) {
-                fieldType = CtClass.shortType;
+                fieldType = pool.get("java.lang.Long");
             } else if (value instanceof OtpErlangInt) {
-                fieldType = CtClass.intType;
+                fieldType = pool.get("java.lang.Long");
             } else if (value instanceof OtpErlangUInt) {
-                fieldType = CtClass.intType;
+                fieldType = pool.get("java.lang.Long");
             } else if (value instanceof OtpErlangLong) {
-                fieldType = CtClass.longType;
+                fieldType = pool.get("java.lang.Long");
             } else if (value instanceof OtpErlangBinary) {
                 fieldType = pool.get("java.lang.String");
             } else if (value instanceof OtpErlangString) {
@@ -117,8 +113,95 @@ public class Main {
         mbeanClazz.toClass();
     }
 
-    private static Riak update(Riak mbean) {
+    private static Riak createAndRegisterMBean() throws Exception {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.basho.riak:type=Riak");
+        RiakMBean mbean = new Riak();
+        mbs.registerMBean(mbean, name);
+
+        // Let's debug MBean!
+        Method[] allMethods = mbean.getClass().getDeclaredMethods();
+        for (Method m : allMethods) {
+            System.out.println("Method: " + m.getName());
+            Class[] p = m.getParameterTypes();
+            if(p.length == 1) {                
+                System.out.println("          " + p[0].getName());
+            }
+        }    
+
+        return (Riak)mbean;
+    }
+
+    private static Riak update(Riak mbean, OtpConnection connection) throws Exception{
+        OtpErlangList stats = getStats(connection);
+        Iterator<OtpErlangObject> it = stats.iterator();
+        while(it.hasNext()) {
+            OtpErlangTuple tuple = (OtpErlangTuple)it.next();
+            OtpErlangAtom name = (OtpErlangAtom)tuple.elementAt(0);
+            OtpErlangObject value = tuple.elementAt(1);
+
+
+            Class fieldType = java.lang.Object.class;
+            Object val = null;
+
+            if(value instanceof OtpErlangFloat) {
+                fieldType = java.lang.Float.class;
+                val = ((OtpErlangFloat)value).floatValue();
+            } else if (value instanceof OtpErlangDouble) {
+                fieldType = java.lang.Double.class;
+                val = ((OtpErlangDouble)value).doubleValue();
+            } else if (value instanceof OtpErlangByte) {
+                fieldType = java.lang.Byte.class;
+                val = ((OtpErlangByte)value).byteValue();
+            } else if (value instanceof OtpErlangChar) {
+                fieldType = java.lang.Character.class;
+                val = ((OtpErlangChar)value).charValue();
+            } else if (value instanceof OtpErlangShort) {
+                fieldType = java.lang.Long.class;
+                val = ((OtpErlangShort)value).longValue();
+            } else if (value instanceof OtpErlangUShort) {
+                fieldType = java.lang.Long.class;
+                val = ((OtpErlangUShort)value).longValue();
+            } else if (value instanceof OtpErlangInt) {
+                fieldType = java.lang.Long.class;
+                val = ((OtpErlangInt)value).longValue();
+            } else if (value instanceof OtpErlangUInt) {
+                fieldType = java.lang.Long.class;
+                val = ((OtpErlangUInt)value).longValue();
+            } else if (value instanceof OtpErlangLong) {
+                fieldType = java.lang.Long.class;
+                val = ((OtpErlangLong)value).longValue();
+            } else if (value instanceof OtpErlangBinary) {
+                fieldType = java.lang.String.class;
+                val = new String(((OtpErlangBinary)value).binaryValue());
+            } else if (value instanceof OtpErlangString) {
+                fieldType = java.lang.String.class;
+                val = ((OtpErlangString)value).stringValue();
+            } else if (value instanceof OtpErlangAtom) {
+                fieldType = java.lang.String.class;
+                val = ((OtpErlangAtom)value).atomValue();
+            } else if (value instanceof OtpErlangList) {
+                fieldType = java.util.ArrayList.class;
+                val = new ArrayList();
+                Iterator<OtpErlangObject> listIt = ((OtpErlangList)value).iterator();
+                while(it.hasNext()) {
+                    ((ArrayList)val).add(it.next());
+                }
+                //val = ((OtpErlangDouble)value).doubleValue();
+            } else {
+                System.out.println("oops! " + value.getClass());
+            }
+
+
+            Method setter = mbean.getClass().getDeclaredMethod("set" + name.toString(), new Class[] {fieldType});
+            setter.invoke(mbean, new Object[] {val});
+        }
         return mbean;
+    }
+
+    private static OtpErlangList getStats(OtpConnection connection) throws Exception {
+        connection.sendRPC("riak_jmx", "stats", new OtpErlangList());
+        return (OtpErlangList)connection.receiveRPC(); 
     }
 
 }
